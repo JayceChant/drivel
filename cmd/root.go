8720455b1af048eda68c1cec05a0aa63a6a83911
Jayce S. Chant <jayce.chant@gmail.com>
2020-02-25 16:48:06 +0800

    fix: remove redundant error printing

--- cmd/root.go
+++ cmd/root.go
@@ -34,7 +34,7 @@ examples and usage of using your application. `,
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		// fmt.Println(err)
 		os.Exit(1)
 	}
 }
